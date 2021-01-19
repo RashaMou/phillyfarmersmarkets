a2ed0924a9a1f391b77e21e8b2ca17ba3fc8b67e
Rasha Moumneh <rasha@fastmailteam.com>
2021-01-19 17:30:46 -0500

    setup webpack, babel, react

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './build'),
+    hot: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
