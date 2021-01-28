babba0e9b0621910da44f5e27ef18f4081aa1290
Rasha Moumneh <rasha@fastmailteam.com>
2021-01-28 00:18:02 -0500

    fix modal icon styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,55 +36,75 @@ const Header = ({ markets }) => {
     <header className="header">
       <div className="logo-container">
         <img className="logo" src={logo} alt="Logo" />
-        <InfoModal />
       </div>
-      <div className="select-container">
-        <Select
-          placeholder="Select option"
-          value={value}
-          onChange={handleChange}
-          placeholder="Neigborhoods"
-        >
-          <option
-            value="option1"
-            id={`${selected.option1 ? "selected" : ""}`}
-            className="option"
+      <div className="filters">
+        <div className="select-container">
+          <Select
+            placeholder="Select option"
+            value={value}
+            onChange={handleChange}
+            placeholder="Neigborhoods"
           >
-            Option 1
-          </option>
-          <option value="option2" id={`${selected.option2 ? "selected" : ""}`}>
-            Option 2
-          </option>
-          <option value="option3" id={`${selected.option3 ? "selected" : ""}`}>
-            Option 3
-          </option>
-        </Select>
-        <Select
-          placeholder="Select option"
-          value={value}
-          onChange={handleChange}
-          placeholder="Food Assistance"
-        >
-          <option value="option4" id={`${selected.option4 ? "selected" : ""}`}>
-            Option 4
-          </option>
-          <option value="option5" id={`${selected.option5 ? "selected" : ""}`}>
-            Option 5
-          </option>
-          <option value="option6" id={`${selected.option6 ? "selected" : ""}`}>
-            Option 6
-          </option>
-        </Select>
-        <Select
-          placeholder="Select option"
-          value={value}
-          onChange={handleChange}
-          placeholder="Open Today"
-        >
-          <option value="option7" id={`${selected.option7 ? "selected" : ""}`}>
-            Option 7
-          </option>
-        </Select>
+            <option
+              value="option1"
+              id={`${selected.option1 ? "selected" : ""}`}
+              className="option"
+            >
+              Option 1
+            </option>
+            <option
+              value="option2"
+              id={`${selected.option2 ? "selected" : ""}`}
+            >
+              Option 2
+            </option>
+            <option
+              value="option3"
+              id={`${selected.option3 ? "selected" : ""}`}
+            >
+              Option 3
+            </option>
+          </Select>
+          <Select
+            placeholder="Select option"
+            value={value}
+            onChange={handleChange}
+            placeholder="Food Assistance"
+          >
+            <option
+              value="option4"
+              id={`${selected.option4 ? "selected" : ""}`}
+            >
+              Option 4
+            </option>
+            <option
+              value="option5"
+              id={`${selected.option5 ? "selected" : ""}`}
+            >
+              Option 5
+            </option>
+            <option
+              value="option6"
+              id={`${selected.option6 ? "selected" : ""}`}
+            >
+              Option 6
+            </option>
+          </Select>
+          <Select
+            placeholder="Select option"
+            value={value}
+            onChange={handleChange}
+            placeholder="Open Today"
+          >
+            <option
+              value="option7"
+              id={`${selected.option7 ? "selected" : ""}`}
+            >
+              Option 7
+            </option>
+          </Select>
+        </div>
+        <InfoModal />
       </div>
       {Object.values(selected).includes(true) && (
         <HStack className="tags">
