73dc1c811993f55a551506af156df7411465c43a
Rasha Moumneh <rasha@fastmailteam.com>
2024-06-10 17:03:00 -0400

    Add recenter functionality

---
+++ src/components/FlyToCenter.js
@@ -0,0 +1,24 @@
+import { useMap } from 'react-leaflet';
+
+export const FlyToCenter = ({ center }) => {
+  const map = useMap();
+
+  const flyToCenter = () => {
+    map.flyTo(center, 12, {
+      animate: true,
+      duration: 0.75,
+    });
+  };
+
+  return (
+    <button
+      type="button"
+      className="absolute top-[10px] right-[10px] z-[9999] p-2 mr-10"
+      onClick={flyToCenter}
+    >
+      <img src="aim.png" className="w-9" />
+    </button>
+  );
+};
+
+export default FlyToCenter;
