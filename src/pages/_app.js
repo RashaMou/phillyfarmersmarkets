2eac54c7dcefb355fd23c762413b1f515c960745
Rasha Moumneh <rasha@rasha.dev>
2024-08-21 15:40:23 -0400

    change font in _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css';
+import { Glegoo } from 'next/font/google';
 
-function MyApp({ Component, pageProps }) {
-  return <Component { ...pageProps } />
+const glegoo = Glegoo({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-glegoo',
+  weight: '700'
+})
+
+function MyApp ({ Component, pageProps }) {
+  return (
+    <div className={glegoo.variable}>
+      <Component { ...pageProps } />
+    </div>
+  );
 }
 
 export default MyApp;
